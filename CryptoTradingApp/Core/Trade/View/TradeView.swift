b4803f00c2a535d156758ecb3c8c2aaf3d7e2b18
Nazar <nazarwelkakayew41@gmail.com>
2022-10-13 15:41:35 +0500

    update: updated trade view, fake data

--- CryptoTradingApp/Core/Trade/View/TradeView.swift
+++ CryptoTradingApp/Core/Trade/View/TradeView.swift
@@ -28,69 +28,14 @@ struct TradeView: View {
                 
                 stableOptimalPriceButton
                 
-                HStack(spacing: 20){
-                    VStack{
-                        HStack{
-                            TextField("Buy amount", text: $vm_tradeview.buyAmount)
-                                //.frame(width: 100)
-                                .keyboardType(.decimalPad)
-                            
-                            Text("BTC")
-
-                        }
-                        .foregroundColor(.black.opacity(0.7))
-                        .font(.caption)
-                        .padding(5)
-                        
-                        HStack(spacing: 0){
-                            amountPercentageButton(type: .buy, percentage: 25) {}
-                            amountPercentageButton(type: .buy, percentage: 50) {}
-                            amountPercentageButton(type: .buy, percentage: 75) {}
-                            amountPercentageButton(type: .buy, percentage: 100) {}
-
-                        }
-                        .foregroundColor(.gray.opacity(0.5))
-                        .font(.caption)
-
-                    }
-                    .border(.gray.opacity(0.2))
-                    
-                    VStack{
-                        HStack{
-                            TextField("Sell amount", text: $vm_tradeview.buyAmount)
-//                                .frame(width: 100)
-                                .keyboardType(.decimalPad)
-                            
-                            Text("BTC")
-
-                        }
-                        .foregroundColor(.black.opacity(0.7))
-                        .font(.caption)
-                        .padding(5)
-                        
-                        HStack(spacing: 0){
-                            
-                            amountPercentageButton(type: .sell, percentage: 25) {}
-                            amountPercentageButton(type: .sell, percentage: 50) {}
-                            amountPercentageButton(type: .sell, percentage: 75) {}
-                            amountPercentageButton(type: .sell, percentage: 100) {}
-
-
-                        }
-                        .foregroundColor(.gray.opacity(0.5))
-                        .font(.caption)
-
-                    }
-                    .border(.gray.opacity(0.2))
-                }
-                .padding(.horizontal, 10)
+                enterAmount()
                 
+                buySellButtons()
                 
             }
         }
         .edgesIgnoringSafeArea(.top)
-      //  .padding(.horizontal, 10)
-
+ 
     }
     
 }
@@ -106,7 +51,7 @@ extension TradeView{
     //MARK: top header, spot trading
     @ViewBuilder private var headerTradeTypeView: some View{
         ZStack(alignment: .bottom){
-            Color.gray.opacity(0.2)
+            Color.gray.opacity(0.1)
             Text("Spot")
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
@@ -122,7 +67,7 @@ extension TradeView{
         HStack{
             Image(systemName: "list.triangle")
                 .font(.system(size: 20, weight: .medium))
-
+            
             
             Button {
                 vm_tradeview.showCoinList = true
@@ -130,7 +75,7 @@ extension TradeView{
                 Text("BTC/USDT")
                     .font(.system(size: 20, weight: .medium))
                     .foregroundColor(.black)
-
+                
             }
             
             Text("+23%")
@@ -157,7 +102,7 @@ extension TradeView{
                     .font(.subheadline)
             }
             
-
+            
         }
         .padding(10)
         
@@ -173,7 +118,7 @@ extension TradeView{
                 .padding(.top)
                 .padding(.leading, 10)
         }
-
+        
     }
     
     //MARK: active market simulator
@@ -207,14 +152,14 @@ extension TradeView{
                     Spacer()
                     
                     VStack(alignment: .trailing, spacing: 3){
-
+                        
                         Text("Price")
                             .foregroundColor(.gray)
                             .font(.caption)
                         
                         ForEach(1..<6){_ in
                             let random = String(format: "%.2f", Double.random(in: 19200..<20000)) //Double.random(in: 19200..<20000)
-
+                            
                             Text(random)
                                 .frame(alignment: .leading)
                         }
@@ -226,25 +171,25 @@ extension TradeView{
                 .background(.green.opacity(0.15))
                 
                 
-                                        
+                
                 
                 HStack{
                     VStack(alignment: .leading, spacing: 3){
-
+                        
                         Text("Price")
                             .foregroundColor(.gray)
                             .font(.caption)
                         
                         ForEach(1..<6){_ in
                             let random = String(format: "%.2f", Double.random(in: 19200..<20000)) //Double.random(in: 19200..<20000)
-
+                            
                             Text(random)
                                 .frame(alignment: .leading)
                         }
                     }
-
                     
-
+                    
+                    
                     
                     VStack(alignment: .trailing, spacing: 3){
                         
@@ -277,7 +222,7 @@ extension TradeView{
                 .background(.red.opacity(0.15))
             }
         }
-
+        
     }
     
     //MARK: order type view
@@ -297,6 +242,7 @@ extension TradeView{
         .padding(4)
         .border(.gray.opacity(0.5), width: 0.5)
         .padding(.horizontal, 10)
+        .padding(.top)
     }
     
     //MARK: fake price buttons (stable optimal price)
@@ -319,6 +265,7 @@ extension TradeView{
             .frame(height: 30)
         }
         .padding(.horizontal, 10)
+        .padding(.top)
     }
     
     //MARK: amount percentage button
@@ -330,8 +277,150 @@ extension TradeView{
         }
         .frame(maxWidth: .infinity, alignment: .center)
         .padding(.vertical, 5)
-        .border(.gray.opacity(0.3))
+        .border(.gray.opacity(0.3), width: 0.5)
     }
     
+    @ViewBuilder private func enterAmount()-> some View{
+        HStack(spacing: 20){
+            VStack {
+                VStack{
+                    HStack{
+                        TextField("Buy amount", text: $vm_tradeview.buyAmount)
+                        //.frame(width: 100)
+                            .keyboardType(.decimalPad)
+                        
+                        Text("BTC")
+                        
+                    }
+                    .foregroundColor(.black.opacity(0.7))
+                    .font(.caption)
+                    .padding(5)
+                    .onTapGesture { UIApplication.shared.endEditing() }
+                    
+                    HStack(spacing: 0){
+                        amountPercentageButton(type: .buy, percentage: 25) {}
+                        amountPercentageButton(type: .buy, percentage: 50) {}
+                        amountPercentageButton(type: .buy, percentage: 75) {}
+                        amountPercentageButton(type: .buy, percentage: 100) {}
+                        
+                    }
+                    .foregroundColor(.gray.opacity(0.5))
+                    .font(.caption)
+                    
+                }
+                .border(.gray.opacity(0.2))
+                
+                HStack(spacing: 0){
+                    Text("Available")
+                    Image(systemName: "questionmark.circle")
+                    
+                    Text("50,000.0000 USDT")
+                        .foregroundColor(.black)
+                        .padding(.leading, 5)
+                    
+                    
+                    Spacer()
+                }
+                .foregroundColor(.gray)
+                .font(.system(size: 10, weight: .regular))
+            }
+            
+            VStack {
+                VStack{
+                    HStack{
+                        TextField("Sell amount", text: $vm_tradeview.sellAmount)
+                        //                                .frame(width: 100)
+                            .keyboardType(.decimalPad)
+                        
+                        Text("BTC")
+                        
+                    }
+                    .foregroundColor(.black.opacity(0.7))
+                    .font(.caption)
+                    .padding(5)
+                    .onTapGesture { UIApplication.shared.endEditing() }
+                    
+                    HStack(spacing: 0){
+                        
+                        amountPercentageButton(type: .sell, percentage: 25) {}
+                        amountPercentageButton(type: .sell, percentage: 50) {}
+                        amountPercentageButton(type: .sell, percentage: 75) {}
+                        amountPercentageButton(type: .sell, percentage: 100) {}
+                        
+                        
+                    }
+                    .foregroundColor(.gray.opacity(0.5))
+                    .font(.caption)
+                    
+                    
+                }
+                .border(.gray.opacity(0.2))
+                
+                HStack(spacing: 0){
+                    Text("Available")
+                    Image(systemName: "questionmark.circle")
+                    
+                    Text("0.00000000 USDT")
+                        .foregroundColor(.black)
+                        .padding(.leading, 5)
+                    
+                    
+                    Spacer()
+                }
+                .foregroundColor(.gray)
+                .font(.system(size: 10, weight: .regular))
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.top, 20)
+    }
+    
+    @ViewBuilder private func buySellButtons()-> some View{
+        HStack(spacing: 20){
+            ZStack{
+                Color.green
+                
+                Button {
+                    //
+                } label: {
+                    ZStack{
+                        Color.green
+                        Color.white.opacity(0.3)
+                        
+                        Text("Buy BTC")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .bold))
+                    }
+                    .frame(height: 35)
+                }
+
+            }
+            .frame(height: 35)
+            
+            ZStack{
+                Color.green
+                
+                Button {
+                    //
+                } label: {
+                    ZStack{
+                        Color.red
+                        Color.white.opacity(0.3)
+
+                        
+                        Text("Sell BTC")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .bold))
+                    }
+                    .frame(height: 35)
+                }
+
+            }
+            .frame(height: 35)
+            
+        }
+        .padding(.horizontal, 10)
+        
+    }
     
 }
